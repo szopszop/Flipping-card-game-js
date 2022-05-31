cf16c99c876c51bad5405a52613f2c4fa2a7a449
Maciej Strękowski <maciejstrekowski@gmail.com>
2022-05-31 09:22:31 +0200

    Add core play

--- game.js
+++ game.js
@@ -1,7 +1,50 @@
-initGame();
+const cards = document.querySelectorAll('.game-card');
 
-function initGame() {
+let hasFlipped = false;
+let blockBoard = false;
+let firstCard, secondCard;
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+function flipCard() {
+    if (blockBoard) return;
+    if (this===firstCard) return;
+
+    this.classList.toggle('flip');
+
+    if (!hasFlipped) {
+        hasFlipped = true;
+        firstCard = this;
+    } else {
+        secondCard = this;
+
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener('click',flipCard);
+            secondCard.removeEventListener('click',flipCard);
+            resetBoard();
+        } else {
+            blockBoard = true;
+            setTimeout(() => {
+                firstCard.classList.remove('flip')
+                secondCard.classList.remove('flip')
+                resetBoard();
+            }, 1200)
+        }
+    }
+    function resetBoard() {
+        [hasFlipped, blockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
 
 }
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+// initGame();
+//
+// function initGame() {
+//
+//     Your game can start here, but define separate functions, don't write everything in here :)
+//
+// }
