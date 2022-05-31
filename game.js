76acaec9bd1373059e3f8184fda2d1eff66f98f9
Maciej Strękowski <maciejstrekowski@gmail.com>
2022-05-31 11:04:54 +0200

    Add refactor + card randomizing

--- game.js
+++ game.js
@@ -15,31 +15,50 @@ function flipCard() {
         firstCard = this;
     } else {
         secondCard = this;
-
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            firstCard.removeEventListener('click',flipCard);
-            secondCard.removeEventListener('click',flipCard);
-            resetBoard();
-        } else {
-            blockBoard = true;
-            setTimeout(() => {
-                firstCard.classList.remove('flip')
-                secondCard.classList.remove('flip')
-                resetBoard();
-            }, 1200)
-        }
+        matchCheck();
     }
     function resetBoard() {
         [hasFlipped, blockBoard] = [false, false];
         [firstCard, secondCard] = [null, null];
     }
-
 }
 
+function matchCheck () {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        freezeCards();
+    } else {
+        restoreCards();
+    }
+}
+
+function freezeCards() {
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+    resetBoard();
+}
+
+function restoreCards() {
+    blockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+        resetBoard();
+        }, 1200)
+}
+
+function resetBoard() {
+    [hasFlipped, blockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]
+}
+
+(function randomizeCards() {
+    cards.forEach(card => {
+        let position = Math.floor(Math.random() * 24);
+        card.style.order = position;
+    })
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
-
 
 // initGame();
 //
